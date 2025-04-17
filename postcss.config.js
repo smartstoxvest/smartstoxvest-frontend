2668681e8bc726c4a39d713af767ac8f001fa1aa
smartstoxvest-org-admin <smartstoxvest@gmail.com>
2025-04-17 19:46:05 +0100

    ✅ Added postcss-preset-env to fix Netlify build issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+// postcss.config.js
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      stage: 3,
+      features: {
+        'nesting-rules': false,
+      },
+    }),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
